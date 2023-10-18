855e434ad151c83eb05bbcb3cb43e2a30877c3db
Greg <80447308+GregBDesign@users.noreply.github.com>
2023-10-18 19:50:18 +1100

    Add min heap tests

--- 3/src/test/java/minheap/MinHeapTest.java
+++ 3/src/test/java/minheap/MinHeapTest.java
@@ -9,7 +9,7 @@ class MinHeapTest {
     MinHeap minHeap = new MinHeap();
 
     @Test
-    void minHeapTestInsertAndDeleteValues() {
+    void minHeapTestInsertAndDeleteValues_evenElems() {
         TreeLeafNode C = new TreeLeafNode('C', 10);
         TreeLeafNode D = new TreeLeafNode('D', 20);
         TreeLeafNode F = new TreeLeafNode('F', 19);
@@ -30,6 +30,32 @@ class MinHeapTest {
         assertEquals(C, minHeap.delete());
         assertEquals(G, minHeap.delete());
         assertEquals(2, minHeap.length);
+    }
+
+    @Test
+    void minHeapTestInsertAndDeleteValues_oddElems() {
+        TreeLeafNode C = new TreeLeafNode('C', 10);
+        TreeLeafNode D = new TreeLeafNode('D', 20);
+        TreeLeafNode F = new TreeLeafNode('F', 19);
+        TreeLeafNode X = new TreeLeafNode('X', 2);
+        TreeLeafNode P = new TreeLeafNode('P', 5);
+        TreeLeafNode G = new TreeLeafNode('G', 12);
+        TreeLeafNode M = new TreeLeafNode('M', 100);
+
+        minHeap.insertValue(C);
+        minHeap.insertValue(D);
+        minHeap.insertValue(F);
+        minHeap.insertValue(X);
+        minHeap.insertValue(P);
+        minHeap.insertValue(G);
+        minHeap.insertValue(M);
+
+        assertEquals(7, minHeap.length);
+        assertEquals(X, minHeap.delete());
+        assertEquals(P, minHeap.delete());
+        assertEquals(C, minHeap.delete());
+        assertEquals(G, minHeap.delete());
+        assertEquals(3, minHeap.length);
     }
 
 }
