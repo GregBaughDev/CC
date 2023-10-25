3089d4b1d44e4b2ce8dcd432e2084a9b43c6048c
Greg <80447308+GregBDesign@users.noreply.github.com>
2023-10-25 21:03:23 +1100

    Write header to file

--- 3/src/main/java/compression/Compression.java
+++ 3/src/main/java/compression/Compression.java
@@ -2,20 +2,25 @@ package compression;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Compression {
-    File file;
+    File fileToParse;
+    File outputFile;
+    PrintWriter writer;
 
-    public Compression(String filename) {
-        this.file = new File(filename);
+    public Compression(String fileToParse, String outputFile) throws FileNotFoundException {
+        this.fileToParse = new File(fileToParse);
+        this.outputFile = new File(outputFile);
+        this.writer = new PrintWriter(outputFile);
     }
 
     public Map<Character, Integer> readFile() throws FileNotFoundException {
         HashMap<Character, Integer> charResults = new HashMap<>();
-        try (Scanner s = new Scanner(file)) {
+        try (Scanner s = new Scanner(fileToParse)) {
             while (s.hasNext()) {
                 char[] tmp = s.nextLine().toCharArray();
                 for (char c : tmp) {
@@ -28,5 +33,24 @@ public class Compression {
             }
         }
         return charResults;
+    }
+
+    public void writeToOutputFile(Map<Character, String> prefixMap) {
+        writeFileHeader(prefixMap);
+
+        // now need a similar method to read file which reads the file and gets each character
+        // we get the character from the map and append it to the file
+
+
+        writer.close();
+    }
+
+    private void writeFileHeader(Map<Character, String> prefixMap) {
+        writer.println("FILE_HEADER_BEGIN");
+        writer.println("| CHAR | PREFIX");
+        prefixMap.forEach((k, v) -> {
+            writer.println("| " + k + " | " + v);
+        });
+        writer.println("FILE_HEADER_END");
     }
 }
