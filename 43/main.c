6c54d3f8a15f8e7103fe6bfcb83c94eee46c367c
Greg <80447308+GregBDesign@users.noreply.github.com>
2024-03-22 20:28:58 +1100

    Initial commit

---
+++ 43/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "raylib.h"
+
+static const int WINDOW_WIDTH = 800;
+static const int WINDOW_HEIGHT = 450;
+
+int main(void) {
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Tetris!");
+
+    return 0;
+}
