526f359e7e07a6a06328dec464f229134fbb805b
Greg <80447308+GregBDesign@users.noreply.github.com>
2024-03-23 15:30:02 +1100

    Start main menu

---
+++ 43/README.md
@@ -0,0 +1,12 @@
+# TO DO:
+- NEXT TIME: 
+    - Button interactions
+    - Start to modularise code
+
+- Step 1:
+    - Fix Makefile
+    - Create main menu
+        - Move everything into an init function and call aftere BeginDrawing();
+    - Store high scores from a file
+    - Play button
+    - Help button which opens new window
--- 43/main.c
+++ 43/main.c
@@ -1,11 +1,43 @@
 #include <stdio.h>
 #include "raylib.h"
 
+static const Color BG_COLOUR = BLACK;
+static const Color TEXT_COLOUR = WHITE;
 static const int WINDOW_WIDTH = 800;
-static const int WINDOW_HEIGHT = 450;
+static const int WINDOW_HEIGHT = 650;
+const int centrePointButton = (WINDOW_WIDTH / 2) - 150;
+const Rectangle button = { .x = centrePointButton, .y = 150, .width = 300, .height = 50};
+const Rectangle button2 = { .x = centrePointButton, .y = 250, .width = 300, .height = 50};
+const char* MAINMENU_WELCOME = "Welcome to Tetris";
+const char* MAINMENU_START = "Start";
+const char* MAINMENU_HELP = "Help";
+const int FONT_SIZE = 30;
+const int FONT_SIZE_SECONDARY = 20;
 
-int main(void) {
-    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Tetris!");
+int main(void) 
+{
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Tetris");
+
+    int strMeasure = MeasureText(MAINMENU_WELCOME, FONT_SIZE) / 2;
+    int centrePoint = (WINDOW_WIDTH / 2) - strMeasure; 
+    int startButtonStrMeasure = MeasureText(MAINMENU_START, FONT_SIZE_SECONDARY) / 2;
+    int startButtonCentrePoint = (WINDOW_WIDTH / 2) - startButtonStrMeasure;
+    int helpButtonStrMeasure = MeasureText(MAINMENU_HELP, FONT_SIZE_SECONDARY) / 2;
+    int helpButtonCentrePoint = (WINDOW_WIDTH / 2) - helpButtonStrMeasure;
+
+    while (!WindowShouldClose()) 
+    {
+        BeginDrawing();
+        ClearBackground(BG_COLOUR);
+        DrawText(MAINMENU_WELCOME, centrePoint, 50, 30, TEXT_COLOUR);
+        DrawRectangleRoundedLines(button, 2, 0, 2, TEXT_COLOUR);
+        DrawText(MAINMENU_START, startButtonCentrePoint, 165, 20, TEXT_COLOUR);
+        DrawRectangleRoundedLines(button2, 2, 0, 2, TEXT_COLOUR);
+        DrawText(MAINMENU_HELP, helpButtonCentrePoint, 265, 20, TEXT_COLOUR);
+        EndDrawing();
+    }
+
+    CloseWindow();
 
     return 0;
 }
